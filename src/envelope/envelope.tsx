26d1a109e85c4ff1fd4b52a0c9b8c787ac0d01f9
AwSicker <vitja1234@gmail.com>
2021-12-05 11:43:17 +0200

    envelope test

--- src/envelope/envelope.tsx
+++ src/envelope/envelope.tsx
@@ -2,7 +2,11 @@ import React, {useEffect, useRef, useState} from 'react';
 import {CSSTransition} from 'react-transition-group'
 import {gsap, Power1} from 'gsap'
 import {useParams} from 'react-router-dom';
+import {InviteTitle} from "./svg/inviteTitle";
 import './envelope.scss'
+import {InviteText} from "./svg/InviteText";
+import ReactCardFlip from 'react-card-flip';
+
 
 export const Envelope = () => {
     const [bShowInfo, setBShowInfo] = useState(true)
@@ -15,6 +19,9 @@ export const Envelope = () => {
     const getGuestNames = () => {
         return guest?.split('-').join(' ');
     }
+
+    const [toggled, setToggled] = useState(false);
+
 
     useEffect(() => {
         // @ts-ignore
@@ -33,7 +40,6 @@ export const Envelope = () => {
                 ease: Power1.easeInOut,
                 bottom: "243px",
                 scale: 0.95,
-                boxShadow: "0 2px 5px 0 rgb(120 116 168 / 42%)",
             },
             0.5
         )
@@ -45,7 +51,7 @@ export const Envelope = () => {
         .to(card,
             {
                 ease: Power1.easeInOut,
-                scale: 2,
+                scale: 2.5,
             },
             2.5
         )
@@ -56,7 +62,7 @@ export const Envelope = () => {
                 bottom: "25px",
                 scale: 1,
                 duration: 0.5,
-                rotation: 90
+                rotation: 0
             },
             1.5
         );
@@ -90,13 +96,12 @@ export const Envelope = () => {
                     </div>
                 </CSSTransition>
                 <div className="base"/>
-                <div className="card" ref={(el:any) => (card = el)}>
-                    <h2>{getGuestNames()}</h2>
-                    <svg onClick={close} className="close" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                        <path d="M0 0h24v24H0V0z" fill="none"/>
-                        <path
-                            d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"/>
-                    </svg>
+                <div onClick={() => setToggled(!toggled)} className="card" ref={(el:any) => (card = el)}>
+                <ReactCardFlip isFlipped={toggled} flipDirection="horizontal" flipSpeedFrontToBack={1.5} flipSpeedBackToFront={1.5}>
+                    <InviteTitle/>
+                    <InviteText/>
+                </ReactCardFlip>
+
                 </div>
                 <svg onClick={play} className="env" viewBox="0 0 668 541" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g id="Group 1">
